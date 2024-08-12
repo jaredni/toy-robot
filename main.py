f97bcaa6f6ed29febfbe8dffdd57d519317b4e6e
Jared Vicentillo <jared.vicentillo@gmail.com>
2024-08-12 09:49:22 +0800

    feature: save robot in json

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ from schema import PlaceSchema
 app = FastAPI()
 r = redis.Redis(host='redis', port=6379, db=0)
 
+def read_robot():
+    robot = open("robot.json", "r")
+    data = json.load(robot)
 
-def get_robot_from_cache():
-    robot = r.get('toy-robot')
-
-    if robot is None:
+    if not data:
         raise HTTPException(status_code=404, detail="No robot placed yet.")
 
-    return json.loads(robot)
+    return json.loads(data)
 
 
-def set_robot(data: dict):
-    r.set('toy-robot', data)
+def set_robot(data):
+    robot = open("robot.json", "w")
+    json.dump(data, robot)
 
 
 @app.post("/place/")
@@ -33,25 +34,25 @@ def place_robot(place: PlaceSchema):
 
 @app.post("/left/", status_code=204)
 def face_left():
-    data = get_robot_from_cache()
+    data = read_robot()
 
     robot = Robot(**data)
     robot.face_left()
-    r.set('toy-robot', robot.get_robot().model_dump_json())
+    set_robot(robot.get_robot().model_dump_json())
 
 
 @app.post("/right/", status_code=204)
 def face_right():
-    data = get_robot_from_cache()
+    data = read_robot()
 
     robot = Robot(**data)
     robot.face_right()
-    r.set('toy-robot', robot.get_robot().model_dump_json())
+    set_robot(robot.get_robot().model_dump_json())
 
 
 @app.post("/move/", status_code=204)
 def move_robot():
-    data = get_robot_from_cache()
+    data = read_robot()
 
     robot = Robot(**data)
     robot.move()
@@ -66,5 +67,5 @@ def move_robot():
 
 @app.get("/report/")
 def get_report():
-    data = get_robot_from_cache()
+    data = read_robot()
     return PlaceSchema(**data).model_dump(exclude={'orientation_value'})
